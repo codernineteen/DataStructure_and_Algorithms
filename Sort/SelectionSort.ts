cdb83268d2f87e68e262824018e24e7ce5b48646
codernineteen <wjsdPcks34@gmail.com>
2022-09-17 12:18:26 +0900

    There is a bug in selection sort(initialized min index variable outside of loop scope), i fixed it

--- Sort/SelectionSort.ts
+++ Sort/SelectionSort.ts
@@ -2,7 +2,6 @@ const unsortedArr2 = [4, 2, 7, 1, 3];
 
 let steps2 = 0;
 let minIndex = 0;
-let startPoint = 0;
 
 for (let i = 0; i < unsortedArr2.length; i++) {
   for (let j = i + 1; j < unsortedArr2.length; j++) {
@@ -13,7 +12,7 @@ for (let i = 0; i < unsortedArr2.length; i++) {
     steps2++;
   }
 
-  if (minIndex != i) {
+  if (minIndex !== i) {
     let tmp = unsortedArr2[minIndex];
     unsortedArr2[minIndex] = unsortedArr2[i];
     unsortedArr2[i] = tmp;
